3ff5afd621a2902257e0c2529939745d0e323ad1
thunderphong <thunderthunderphong@gmail.com>
2020-07-29 15:17:23 +0700

    change taskmanager app

---
+++ task-manager/src/routers/user.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const User = require('../models/user')
+const router = new express.Router();
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save(); 
+        res.status(201).send(user)
+    } 
+    catch (err) { res.status(400).send() }
+})
+
+router.get('/users', async (req, res) =>{
+    try { 
+        const users = await User.find({});
+        res.status(200).send(users);
+    }
+    catch (err) { res.status(500).send() }
+})
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id;
+    try { 
+        const user = await User.findById(_id);
+        if (!user) res.status(404).send();
+        res.send(user);
+    }
+    catch (err) { res.status(500).send() }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    const update = Object.keys(req.body);
+    const allowUpdate = ['name', 'email', 'password', 'age'];
+    const isValidOperate = update.every((key) => allowUpdate.includes(key));
+
+    if(!isValidOperate) return res.status(400).send({err: 'Invalid Update'});
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!user) return res.status(404).send();
+        res.send(user);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) return res.status(404).send();
+        res.send(user);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+})
+
+module.exports = router;
